9a5072b525da1f50b0b939ff058c46c295a2329c
inescrisostomo <96204642+inescrisostomo@users.noreply.github.com>
2022-01-29 01:26:27 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-let swirl =10;
+let swirl =100;
 let s= 200;
+let a=0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);  
@@ -10,14 +11,22 @@ function setup() {
 function draw() {
   background(0);
   rectMode(CENTER);
+  a++;
+  stroke(255);  
+  for(var x=0; x<500; x= x+1){
+   mainCircle();
+   stars();
+   absorption(); 
 
-  stroke(255);    
- for(var x=0; x<200; x= x+1){
-
-mainCircle();
-stars();
-absorption();     
   }
+  stroke(255);
+  fill(255)
+  noStroke();
+  push();
+  pop();  
+  ellipse(centrox,centroy,200,200);
+  fill(0);
+  ellipse(centrox,centroy,199.5,199.5)
 
 }
 
@@ -25,28 +34,20 @@ function mainCircle(){
   fill(255)
   noStroke();
   push();
-  pop();  
- 
-  ellipse(centrox,centroy,200,200);
-  fill(0);
-  ellipse(centrox,centroy,199.5,199.5)
-  
+  pop();    
 }
 
 function stars(){
   fill(random(200));  
   let s1=random(width);
   let s2=random(height);  
-  ellipse(s1,s2, 3,2)
-  
+  ellipse(s1,s2, 2,1);  
 }
-
 
 function absorption() {    
   push();   
   translate(centrox,centroy);
-  stroke('white');
-    
+  stroke('white');    
   strokeWeight(random(10));
   let y = s *cos(swirl/.5);
   let z = s*sin(swirl/.10);
